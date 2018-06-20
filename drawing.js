f852e6eae37f2b79b089f1e842aea2b2325457ac
Nikkia Bourn <nikkia.bourn@scholar.smashprogram.org>
2018-06-20 11:39:08 -0700

    First commit

---
+++ drawing.js
@@ -0,0 +1,26 @@
+function draw() {
+  background(0, 255, 0);
+
+  // head
+  fill(255, 204, 0);
+  ellipse(width / 2, height / 2 - 100, 200, 200);
+
+  // eyes
+  fill(0, 100, 100);
+  ellipse(width / 2 - 40, height / 2 - 110, 20, 20);
+  ellipse(width / 2 + 40, height / 2 - 110, 20, 20);
+
+  // mouth
+  arc(width / 2, height / 2 - 60, 100, 60, radians(0), radians(180));
+
+  // body
+  line(width / 2, height / 2, width / 2, height / 2 + 180);
+
+  // arms
+  line(width / 2, height / 2 + 60, width / 2 - 80, height / 2 - 20);
+  line(width / 2, height / 2 + 60, width / 2 + 80, height / 2 - 20);
+
+  // legs
+  line(width / 2, height / 2 + 180, width / 2 - 80, height / 2 + 220);
+  line(width / 2, height / 2 + 180, width / 2 + 80, height / 2 + 220);
+}
